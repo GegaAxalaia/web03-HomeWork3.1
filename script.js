386720a96aeb550ad5f7e7d5e5637b59c86cdc04
GegaAxalaia <gega.axa@gmail.com>
2023-06-06 21:50:52 +0400

    push

---
+++ script.js
@@ -0,0 +1,20 @@
+//1
+ for (let i = 0; i < 100; i++){
+    console.log(i)
+ }
+  //2
+ let index = 0;
+ while (index < 50){
+    console.log(index);
+    index++;
+ }
+ //3
+ let names = ["john", "gega","luka", "jane","giorgi"]
+ names.pop();
+ console.log(names);
+ names.shift("john");
+ console.log(names);
+ names.push("genadich")
+ console.log(names)
+ names.unshift("zaura")
+ console.log(names)
